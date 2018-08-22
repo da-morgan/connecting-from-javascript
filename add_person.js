52c148e7aeac22633f5781e4ac3bff8bf16408a4
David Morgan <david.alexandermorgan@gmail.com>
2018-08-22 00:26:18 +0000

    Added funtionality that lets users add a person from the db to the command line

---
+++ add_person.js
@@ -0,0 +1,23 @@
+const settings = require("./settings.json");
+const userInput = process.argv[2];
+
+const knex = require('knex')({
+    client: 'pg',
+    version: '7.2',
+    connection: settings
+});
+
+(knex("famous_people").insert(
+    {first_name: process.argv[2],
+    last_name: process.argv[3],
+    birthdate: process.argv[4]}
+).returning("*").asCallback(function(err, rows){
+    if(err){
+        console.log("error", err);
+    }
+    else {
+        console.log("nice");
+    }
+}).finally(function(){
+    knex.destroy();
+}))
--- lookup-people-knex.js
+++ lookup-people-knex.js
@@ -7,6 +7,24 @@ const knex = require('knex')({
     connection: settings
 });
 
+function datePrinter(date){
+    if((date.getMonth() + 1) < 10 && date.getDate() < 10){
+        return date.getFullYear() +'-' + '0' +(date.getMonth() + 1) + '-' + '0' + date.getDate()
+    } else if((date.getMonth() + 1) < 10 && date.getDate() >= 10) {
+        return date.getFullYear() +'-' + '0' +(date.getMonth() + 1) + '-' + date.getDate()
+    } else if((date.getMonth() + 1) >= 10 && date.getDate() < 10) {
+        return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + '0' + date.getDate()
+    } else {
+        return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
+    }
+}
+
 knex.select().table('famous_people').where('first_name', 'like', `%${userInput}%`).orWhere('last_name', 'like', `%${userInput}%`).asCallback(function(err, rows){
-    console.log(rows);
+    console.log(`Found ${rows.length} person(s) by the name ${userInput}.`)
+    rows.forEach((person) => {
+        console.log(`- ${person.id}: ${person.first_name} ${person.last_name}, born ${datePrinter(person.birthdate)}`);
+    })
+}).finally(function(){
+    knex.destroy();
 });
+
